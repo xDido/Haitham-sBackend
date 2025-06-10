7f057a59678f7e28a62f6f0bfefd54f6e6bae1d4
Dido <adhaithaam@gmail.com>
2025-06-10 09:46:33 +0300

    first

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,14 +1,15 @@
-import { Schema, model } from 'mongoose';
+import {Schema, model} from 'mongoose';
 
 const ProjectSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    technologies: { type: [String], required: true, default: [] },
-    githubLink: { type: String },
-    imageUrl: { type: String },
-  },
-  { timestamps: true }
+    {
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        technologies: {type: [String], required: true, default: []},
+        bulletPoints: {type: [String], default: []},
+        githubLink: {type: String},
+        imageUrl: {type: String},
+    },
+    {timestamps: true}
 );
 
 export const ProjectModel = model('Project', ProjectSchema);
